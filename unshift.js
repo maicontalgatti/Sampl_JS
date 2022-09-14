139ea215c368495be518c8dc795efc6c398a7bee
AmplaAutomacao <dionestalgattimaicon@gmail.com>
2022-09-14 12:36:19 -0300

    :rocket: unshift

---
+++ unshift.js
@@ -0,0 +1,9 @@
+
+function constructor_unshift() {
+    var fruits = ["apples", "pears"];
+    fruits.unshift("oranges", "grapes");
+    document.write("<p>( UNSHIFT() )You picked my fruits: " + fruits+"</p>");
+};
+
+constructor_unshift();
+
