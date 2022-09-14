6d62235b8744fbb8acb3056763c7ba4a69ce27d1
AmplaAutomacao <dionestalgattimaicon@gmail.com>
2022-09-14 12:25:53 -0300

    :rocket: shift

---
+++ shift.js
@@ -0,0 +1,9 @@
+
+function constructor_shift() {
+    var fruits = ["oranges", "apples", "pears"];
+    var picked_fruit = fruits.shift();
+    document.write("<p>( SHIFT() )You picked my " + picked_fruit+"</p>");
+};
+
+constructor_shift();
+
