c0a302406a4fe7a8063203594dc3b06e6359dd8d
AmplaAutomacao <dionestalgattimaicon@gmail.com>
2022-09-14 12:25:34 -0300

    :rocket: pop

---
+++ pop.js
@@ -0,0 +1,9 @@
+ 
+function constructor_pop() {
+    var fruits = ["oranges", "apples", "pears"];
+    var fruit_string = fruits.pop();
+    document.write("<p>( POP() )You picked my " + fruit_string+"</p>");
+};
+ 
+constructor_pop();
+
